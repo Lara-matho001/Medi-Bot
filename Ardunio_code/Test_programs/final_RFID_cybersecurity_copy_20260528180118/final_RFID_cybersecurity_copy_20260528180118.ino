62e671bc260edaef68aaf515b06a1b78da9616a9
danieladinugraha <danieladinugraha123@gmail.com>
2026-06-02 17:06:21 +1000

    Update final_RFID_cybersecurity_copy_20260528180118.ino

--- Ardunio_code/Test_programs/final_RFID_cybersecurity_copy_20260528180118/final_RFID_cybersecurity_copy_20260528180118.ino
+++ Ardunio_code/Test_programs/final_RFID_cybersecurity_copy_20260528180118/final_RFID_cybersecurity_copy_20260528180118.ino
@@ -5,21 +5,23 @@
 #define RST_PIN 9
 
 MFRC522 rfid(SS_PIN, RST_PIN);
-MFRC522::MIFARE_Key key;
-
-byte block = 4;
 
 void setup() {
   Serial.begin(9600);
+
+  // Important for Arduino Mega SPI
+  pinMode(53, OUTPUT);
+  digitalWrite(53, HIGH);
+
+  pinMode(SS_PIN, OUTPUT);
+  digitalWrite(SS_PIN, HIGH);
+
   SPI.begin();
   rfid.PCD_Init();
+  delay(500);
 
-  for (byte i = 0; i < 6; i++) {
-    key.keyByte[i] = 0xFF;
-  }
-
-  Serial.println("MEDIBOT RFID UID AUTHENTICATION READY");
-  Serial.println("Tap your RFID band...");
+  rfid.PCD_AntennaOn();
+  rfid.PCD_SetAntennaGain(MFRC522::RxGain_max);
 }
 
 void loop() {
@@ -31,41 +33,24 @@ void loop() {
     return;
   }
 
-  MFRC522::StatusCode status;
+  Serial.print("UID: ");
 
-  status = rfid.PCD_Authenticate(
-    MFRC522::PICC_CMD_MF_AUTH_KEY_A,
-    block,
-    &key,
-    &(rfid.uid)
-  );
-
-  if (status != MFRC522::STATUS_OK) {
-    Serial.println("AUTH_FAILED");
-    stopRFID();
-    delay(1000);
-    return;
-  }
-
-  printUID();
-
-  stopRFID();
-  delay(1000);
-}
-
-void printUID() {
   for (byte i = 0; i < rfid.uid.size; i++) {
     if (rfid.uid.uidByte[i] < 0x10) {
       Serial.print("0");
     }
 
     Serial.print(rfid.uid.uidByte[i], HEX);
+
+    if (i < rfid.uid.size - 1) {
+      Serial.print(" ");
+    }
   }
 
   Serial.println();
-}
 
-void stopRFID() {
   rfid.PICC_HaltA();
   rfid.PCD_StopCrypto1();
+
+  delay(1000);
 }
