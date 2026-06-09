9bfbbcedd457217fed247f2958471f7cc6672415
Lara-matho001 <lmatheson001@gmail.com>
2026-06-09 18:13:32 +1000

    Update pin_config.h

--- arduino_code/tests/stepper_position/pin_config.h
+++ arduino_code/tests/stepper_position/pin_config.h
@@ -4,11 +4,11 @@
 #include <Arduino.h>
 
 // Change stepper and sensor wiring here.
-const byte STEPPER_STEP_PIN = 3;
-const byte STEPPER_DIR_PIN = 4;
-const byte STEPPER_ENABLE_PIN = 8;
+const byte STEPPER_STEP_PIN = 23;
+const byte STEPPER_DIR_PIN = 22;
+const byte STEPPER_ENABLE_PIN = 24;
 
 // Home IR sensor. Uses INPUT_PULLUP, so LOW means the home marker is detected.
-const byte HOME_SENSOR_PIN = 7;
+const byte HOME_SENSOR_PIN = ;
 
 #endif
